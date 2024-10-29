dd1f2a6be2cda9bf75b01fbf331ef53850ad6eff
sktman911 <chiemducgiang@gmail.com>
2024-10-29 14:25:10 +0700

    update v2.4 m1

--- FurnitureAPI/FurnitureAPI/Controllers/PaymentController.cs
+++ FurnitureAPI/FurnitureAPI/Controllers/PaymentController.cs
@@ -32,13 +32,13 @@ namespace FurnitureAPI.Controllers
             var res = _momoService.PaymentExcute(Request.Query);
             if (res.ResultCode == null || res == null)
             {
-                return Redirect("https://furniturestore-fct8.onrender.com/paymentReturn?success=false");
+                return Redirect("https://furniture-store-tawny.vercel.app/paymentReturn?success=false");
             }
             try
             {
                 var order = MomoService.GetOrder();
                 await _orderService.AddOrder(order);
-                return Redirect("https://furniturestore-fct8.onrender.com/paymentReturn?success=true");
+                return Redirect("https://furniture-store-tawny.vercel.app/paymentReturn?success=true");
             }
             catch (BadHttpRequestException e)
             {
@@ -59,14 +59,14 @@ namespace FurnitureAPI.Controllers
 
             if (res == null || res.VnPayResponseCode != "00")
             {
-                return Redirect("https://furniturestore-fct8.onrender.com/paymentReturn?success=false");
+                return Redirect("https://furniture-store-tawny.vercel.app/paymentReturn?success=false");
             }
        
             try
             {
                 var order = res.Order;
                 await _orderService.AddOrder(order!);
-                return Redirect("https://furniturestore-fct8.onrender.com/paymentReturn?success=true");
+                return Redirect("https://furniture-store-tawny.vercel.app/paymentReturn?success=true");
             }
             catch (BadHttpRequestException e)
             {
