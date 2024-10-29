27f8cdcffc5df7107d533b052253766c8264df3c
sktman911 <chiemducgiang@gmail.com>
2024-10-29 18:05:11 +0700

    update v2.4 m3

--- FE/src/index.js
+++ FE/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react"
 
 import ShopContextProvider from "./context/ShopContext";
 import { StateContextProvider } from "./context/StateContext";
@@ -16,6 +17,7 @@ import store from "./slice/store"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Analytics/>
     <Provider store={store}>
       <ShopContextProvider>
         <StateContextProvider>
